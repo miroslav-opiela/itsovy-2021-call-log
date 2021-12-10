6dd945c2781d500df6ec8e385ddc5c0743ce1c24
miroslav-opiela <miroslav-opiela@upjs.sk>
2021-12-10 12:21:26 +0100

    adapter

--- app/src/main/java/sk/itsovy/android/calllog/CallLogAdapter.kt
+++ app/src/main/java/sk/itsovy/android/calllog/CallLogAdapter.kt
@@ -1,7 +1,9 @@
 package sk.itsovy.android.calllog
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -10,24 +12,30 @@ class CallLogAdapter : ListAdapter<Call, CallLogAdapter.ViewHolder>(DiffCallback
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
+        private val textView : TextView = itemView.findViewById(android.R.id.text1)
+
+        fun bind(item: Call) {
+            textView.text = item.number
+        }
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+       val layout = LayoutInflater.from(parent.context)
+           .inflate(android.R.layout.simple_list_item_1, parent, false)
+        return ViewHolder(layout)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(getItem(position))
     }
 
-    object DiffCallback: DiffUtil.ItemCallback<Call>() {
-        override fun areItemsTheSame(oldItem: Call, newItem: Call): Boolean {
-            TODO("Not yet implemented")
-        }
+    object DiffCallback : DiffUtil.ItemCallback<Call>() {
+        override fun areItemsTheSame(oldItem: Call, newItem: Call): Boolean =
+            oldItem.number == newItem.number
 
-        override fun areContentsTheSame(oldItem: Call, newItem: Call): Boolean {
-            TODO("Not yet implemented")
-        }
+        override fun areContentsTheSame(oldItem: Call, newItem: Call): Boolean =
+            oldItem == newItem
 
     }
 
