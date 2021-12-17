13032c112a28376211182e72daeb720be612c9aa
miroslav-opiela <miroslav.opiela@upjs.sk>
2021-12-17 03:40:08 +0100

    transakcie fragmentov

--- app/src/main/java/sk/itsovy/android/calllog/MainActivity.kt
+++ app/src/main/java/sk/itsovy/android/calllog/MainActivity.kt
@@ -2,10 +2,33 @@ package sk.itsovy.android.calllog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
+import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity() {
+
+    val model: SharedViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // ak ide o portrait rezim tak potrebujem nastavit prislusny fragment
+        val isPortrait = findViewById<View>(R.id.activity_portrait) != null
+        if (isPortrait) {
+            supportFragmentManager.commit {
+                replace(R.id.activity_portrait, MasterFragment())
+            }
+
+            model.selectedCall.observe(this) {
+               supportFragmentManager.commit {
+                   replace(R.id.activity_portrait, DetailFragment())
+                   addToBackStack(null)
+               }
+            }
+        }
+
+
     }
 }
